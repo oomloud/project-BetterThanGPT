959dbac0613e5ceb7ccda9a4b586f2dc19b9b597
Serhiy Chepa <sergei.chepa@gmail.com>
2023-06-03 23:40:35 +0200

    [Add] added import of JS scripts to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,1 @@
-(() => {
-  const refs = {
-    openMobileBtn: document.querySelector("[data-mobile-menu-open]"),
-    closeMobileBtn: document.querySelector("[data-mobile-menu-close]"),
-    mobile: document.querySelector("[data-mobile-menu]"),
-  };
-
-  refs.openMobileBtn.addEventListener("click", toggleModal);
-  refs.closeMobileBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.mobile.classList.toggle("is-hidden");
-  }
-})();
+import './mobile_menu';
